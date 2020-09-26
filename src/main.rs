bcbcc498636e4efa9311bc55c52acf83d88087b8
guimcaballero <guim.caballero@gmail.com>
2020-09-26 13:45:46 +0200

    Add scene management

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![feature(clamp)]
 #![feature(tau_constant)]
 
-use rand::{rngs::*, Rng};
+use rand::Rng;
 use rayon::prelude::*;
 use std::fs::File;
 use std::io::prelude::*;
@@ -9,7 +9,6 @@ use std::path::Path;
 use std::time::Instant;
 
 mod perlin;
-use perlin::*;
 mod vec3;
 use vec3::*;
 mod ray;
@@ -18,18 +17,17 @@ mod hittable;
 use hittable::*;
 mod hit_record;
 use hit_record::*;
-mod hittable_list;
-use hittable_list::*;
 mod camera;
+mod hittable_list;
 use camera::*;
-mod material;
-use material::*;
 mod aabb;
-use aabb::*;
 mod bvh;
-use bvh::*;
+mod material;
+mod scenes;
 mod texture;
-use texture::*;
+use scenes::*;
+
+const SCENE: Scene = Scene::TwoPerlinSpheres;
 
 fn main() {
     let start = Instant::now();
@@ -56,32 +54,13 @@ fn main() {
 fn get_image_string() -> String {
     // Image
     let aspect_ratio = 3.0 / 2.0;
-    let image_width = 400;
+    let image_width = 1200;
     let image_height = (image_width as f64 / aspect_ratio) as u16;
     let samples_per_pixel: u16 = 50;
     let max_depth: u16 = 50;
 
-    // Camera
-    let lookfrom = Point::new(13.0, 2.0, 3.0);
-    let lookat = Point::new(0.0, 0.0, 0.0);
-    let dist_to_focus = 10.0; //(lookfrom - lookat).length();
-    let vfov = 20.0;
-    let aperture = 0.1;
-
-    let camera = Camera::new(
-        lookfrom,
-        lookat,
-        Vec3::new(0.0, 1.0, 0.0),
-        vfov,
-        aspect_ratio,
-        aperture,
-        dist_to_focus,
-        0.0,
-        1.0,
-    );
-
     // World
-    let world = generate_world();
+    let (world, camera) = scenes::generate_world(SCENE, aspect_ratio);
 
     let string = (0..image_height)
         .into_par_iter()
@@ -137,86 +116,4 @@ fn ray_color(ray: &Ray, world: &dyn Hittable, depth: u16) -> Color {
     let unit = ray.direction.normalize();
     let t = 0.5 * (unit.y + 1.0);
     (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.3, 0.3, 1.0)
-}
-
-fn generate_world() -> HittableList {
-    let mut world = HittableList::new();
-
-    // Ground
-    let checker = texture::checker(
-        texture::solid_color(Color::new(0.2, 0.3, 0.1)),
-        texture::solid_color(Color::new(0.9, 0.9, 0.9)),
-    );
-    world.add(Box::new(Sphere {
-        center: Point::new(0.0, -1000.0, 0.0),
-        radius: 1000.0,
-        material: Material::Lambertian(checker),
-    }));
-
-    // Spheres
-    world.add(Box::new(Sphere {
-        center: Point::new(0.0, 1.0, 0.0),
-        radius: 1.0,
-        material: Material::Dielectric(1.5),
-    }));
-    world.add(Box::new(Sphere {
-        center: Point::new(-4.0, 1.0, 0.0),
-        radius: 1.0,
-        material: Material::Lambertian(texture::noise(Perlin::new())),
-    }));
-    world.add(Box::new(Sphere {
-        center: Point::new(4.0, 1.0, 0.0),
-        radius: 1.0,
-        material: Material::Metal(Color::new(0.8, 0.6, 0.2), 0.0),
-    }));
-
-    // Illuminated sphere
-    world.add(Box::new(Sphere {
-        center: Point::new(-4.0, 0.5, 2.0),
-        radius: 0.5,
-        material: Material::Lambertian(texture::solid_color(Color::new(2.0, 2.0, 1.0))),
-    }));
-
-    for a in -5..5 {
-        for b in -5..5 {
-            let choose_mat = rand::thread_rng().gen::<f64>();
-            let center = Point {
-                x: a as f64 + 0.9 * rand::thread_rng().gen::<f64>(),
-                y: 0.2,
-                z: b as f64 + 0.9 * rand::thread_rng().gen::<f64>(),
-            };
-
-            if (center - Vec3::new(4.0, 0.2, 0.0)).length() > 0.9 {
-                if choose_mat < 0.8 {
-                    let albedo = Color::random() * Color::random();
-                    let center1 =
-                        center + Vec3::new(0.0, rand::thread_rng().gen_range(0.0, 0.3), 0.0);
-                    world.add(Box::new(MovingSphere {
-                        center0: center,
-                        center1,
-                        time0: 0.0,
-                        time1: 1.0,
-                        radius: 0.2,
-                        material: Material::Lambertian(texture::solid_color(albedo)),
-                    }));
-                } else if choose_mat < 0.95 {
-                    let albedo = Color::random_range(0.5, 1.0);
-                    let fuzz = rand::thread_rng().gen_range(0.0, 0.5);
-                    world.add(Box::new(Sphere {
-                        center,
-                        radius: 0.2,
-                        material: Material::Metal(albedo, fuzz),
-                    }));
-                } else {
-                    world.add(Box::new(Sphere {
-                        center,
-                        radius: 0.2,
-                        material: Material::Dielectric(1.5),
-                    }));
-                }
-            }
-        }
-    }
-
-    world
 }
---
+++ src/scenes.rs
@@ -0,0 +1,154 @@
+use crate::{
+    bvh::*, camera::*, hittable::*, hittable_list::*, material::*, perlin::*, texture, vec3::*,
+};
+use rand::Rng;
+
+pub enum Scene {
+    ManySpheres,
+    TwoPerlinSpheres,
+}
+
+pub fn generate_world(scene: Scene, aspect_ratio: f64) -> (HittableList, Camera) {
+    match scene {
+        Scene::ManySpheres => many_spheres(aspect_ratio),
+        Scene::TwoPerlinSpheres => two_perlin_spheres(aspect_ratio),
+    }
+}
+
+fn many_spheres(aspect_ratio: f64) -> (HittableList, Camera) {
+    let mut world = HittableList::new();
+
+    // Ground
+    let checker = texture::checker(
+        texture::solid_color(Color::new(0.2, 0.3, 0.1)),
+        texture::solid_color(Color::new(0.9, 0.9, 0.9)),
+    );
+    world.add(Box::new(Sphere {
+        center: Point::new(0.0, -1000.0, 0.0),
+        radius: 1000.0,
+        material: Material::Lambertian(checker),
+    }));
+
+    // Spheres
+    world.add(Box::new(Sphere {
+        center: Point::new(0.0, 1.0, 0.0),
+        radius: 1.0,
+        material: Material::Dielectric(1.5),
+    }));
+    world.add(Box::new(Sphere {
+        center: Point::new(-4.0, 1.0, 0.0),
+        radius: 1.0,
+        material: Material::Lambertian(texture::noise(Perlin::new())),
+    }));
+    world.add(Box::new(Sphere {
+        center: Point::new(4.0, 1.0, 0.0),
+        radius: 1.0,
+        material: Material::Metal(Color::new(0.8, 0.6, 0.2), 0.0),
+    }));
+
+    // Illuminated sphere
+    world.add(Box::new(Sphere {
+        center: Point::new(-4.0, 0.5, 2.0),
+        radius: 0.5,
+        material: Material::Lambertian(texture::solid_color(Color::new(2.0, 2.0, 1.0))),
+    }));
+
+    for a in -5..5 {
+        for b in -5..5 {
+            let choose_mat = rand::thread_rng().gen::<f64>();
+            let center = Point {
+                x: a as f64 + 0.9 * rand::thread_rng().gen::<f64>(),
+                y: 0.2,
+                z: b as f64 + 0.9 * rand::thread_rng().gen::<f64>(),
+            };
+
+            if (center - Vec3::new(4.0, 0.2, 0.0)).length() > 0.9 {
+                if choose_mat < 0.8 {
+                    let albedo = Color::random() * Color::random();
+                    let center1 =
+                        center + Vec3::new(0.0, rand::thread_rng().gen_range(0.0, 0.3), 0.0);
+                    world.add(Box::new(MovingSphere {
+                        center0: center,
+                        center1,
+                        time0: 0.0,
+                        time1: 1.0,
+                        radius: 0.2,
+                        material: Material::Lambertian(texture::solid_color(albedo)),
+                    }));
+                } else if choose_mat < 0.95 {
+                    let albedo = Color::random_range(0.5, 1.0);
+                    let fuzz = rand::thread_rng().gen_range(0.0, 0.5);
+                    world.add(Box::new(Sphere {
+                        center,
+                        radius: 0.2,
+                        material: Material::Metal(albedo, fuzz),
+                    }));
+                } else {
+                    world.add(Box::new(Sphere {
+                        center,
+                        radius: 0.2,
+                        material: Material::Dielectric(1.5),
+                    }));
+                }
+            }
+        }
+    }
+
+    // Camera
+    let lookfrom = Point::new(13.0, 2.0, 3.0);
+    let lookat = Point::new(0.0, 0.0, 0.0);
+    let dist_to_focus = 10.0; //(lookfrom - lookat).length();
+    let vfov = 20.0;
+    let aperture = 0.1;
+
+    let camera = Camera::new(
+        lookfrom,
+        lookat,
+        Vec3::new(0.0, 1.0, 0.0),
+        vfov,
+        aspect_ratio,
+        aperture,
+        dist_to_focus,
+        0.0,
+        1.0,
+    );
+
+    (world, camera)
+}
+
+fn two_perlin_spheres(aspect_ratio: f64) -> (HittableList, Camera) {
+    let mut world = HittableList::new();
+
+    let texture = texture::noise(Perlin::new());
+    world.add(Box::new(Sphere {
+        center: Point::new(0.0, 2.0, 0.0),
+        radius: 2.0,
+        material: Material::Lambertian(texture.clone()),
+    }));
+    world.add(Box::new(Sphere {
+        center: Point::new(0.0, -1000.0, 0.0),
+        radius: 1000.0,
+        material: Material::Lambertian(texture.clone()),
+    }));
+
+    // Camera
+    let lookfrom = Point::new(13.0, 2.0, 3.0);
+    let lookat = Point::new(0.0, 0.0, 0.0);
+    let dist_to_focus = 10.0;
+    let vfov = 20.0;
+    let aperture = 0.0;
+
+    let camera = Camera::new(
+        lookfrom,
+        lookat,
+        Vec3::new(0.0, 1.0, 0.0),
+        vfov,
+        aspect_ratio,
+        aperture,
+        dist_to_focus,
+        0.0,
+        1.0,
+    );
+
+    (world, camera)
+}
